0d187c40f724143a171ec586a69b2c2939d4a31c
luxs710 <luxs710@gmail.com>
2023-08-12 18:42:52 -0300

    adicionando models

---
+++ src/main/java/com/example/Cadastro/de/disciplinas/CadastroDeDisciplinasApplication.java
@@ -0,0 +1,13 @@
+package com.example.Cadastro.de.disciplinas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeDisciplinasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeDisciplinasApplication.class, args);
+	}
+
+}
